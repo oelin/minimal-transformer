237b590dd86f5731b3594b6710b4da96767b1eb0
oelin <42823429+oelin@users.noreply.github.com>
2023-12-02 12:26:00 +0000

    Update masking.py

--- masking.py
+++ masking.py
@@ -1,21 +1,14 @@
 def get_causal_mask(x: torch.Tensor) -> torch.Tensor:
-  
     sequence_length = x.size(1)
-    mask = (1 - torch.tril(torch.ones((sequence_length, sequence_length)))).float()
 
-    return mask
+    mask = (1 - torch.tril(torch.ones((sequence_length, sequence_length))))
 
+    return mask.bool()
 
-def get_padding_mask(x: torch.Tensor, maximum_sequence_length: int) -> torch.Tensor:
-
-    sequence_length = x.size(1)
-    mask = torch.ones((maximum_sequence_length, maximum_sequence_length))
-    mask[:, : sequence_length] = 0.
-
-    return mask
 
 def convert_mask_from_binary_to_float(mask: torch.Tensor) -> torch.Tensor:
 
+    mask = mask.clone().float()
     mask.masked_fill_(mask.bool(), -torch.inf)
 
     return mask
